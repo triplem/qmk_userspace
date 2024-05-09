eea412d393e4111981fa1e64fa6b59ec5bf53001
triplem <160079+triplem@users.noreply.github.com>
2024-05-09 22:20:01 +0200

    add tap dances, space cadet shift

--- users/triplem/rules.mk
+++ users/triplem/rules.mk
@@ -1,6 +1,10 @@
+SRC += triplem.c
+
 GRAVE_ESC_ENABLE = yes
+SPACE_CADET_ENABLE = yes
+TAP_DANCE_ENABLE = yes
 
 # Reduce compiled size
 VIA_ENABLE = no
 MOUSEKEY_ENABLE = no
-SPACE_CADET_ENABLE = no
+COMMAND_ENABLE = no
