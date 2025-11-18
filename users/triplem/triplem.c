1538080df752466e05dacea55a53ef055e6a1251
triplem <160079+triplem@users.noreply.github.com>
2025-11-18 22:14:33 +0100

    fix: revert umlaut handling to double tap

--- users/triplem/triplem.c
+++ users/triplem/triplem.c
@@ -1,89 +1,26 @@
 #include "triplem.h"
 
-//Tap Dance Definitions
-void uml_tap_dance(tap_dance_state_t *state, void *user_data, uint16_t keycode_first, uint16_t keycode_uml) {
-     if (state->count == 1) {
-        tap_code(keycode_first);
-    } else if (state->count == 2) {
-        tap_code(keycode_first);
-    } else if (state->count == 3) {
-        tap_code16(keycode_uml);
-    } else {
-        reset_tap_dance(state);
-    }
-};
-
-void auml_tap_dance(tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-        tap_code(KC_A);
-    } else if (state->count == 2) {
-        tap_code(KC_A);
-    } else if (state->count == 3) {
-        tap_code16(UML_AE);
-    } else {
-        reset_tap_dance(state);
-    }
-};
-
-void ouml_tap_dance(tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-        tap_code(KC_O);
-    } else if (state->count == 2) {
-        tap_code(KC_O);
-    } else if (state->count == 3) {
-        tap_code16(UML_OE);
-    } else {
-        reset_tap_dance(state);
-    }
-};
-
-void uuml_tap_dance(tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-        tap_code(KC_U);
-    } else if (state->count == 2) {
-        tap_code(KC_U);
-    } else if (state->count == 3) {
-        tap_code16(UML_UE);
-    } else {
-        reset_tap_dance(state);
-    }
-
-};
-
-void ger_sz_tap_dance(tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-        tap_code(KC_S);
-    } else if (state->count == 2) {
-        tap_code(KC_S);
-    } else if (state->count == 3) {
-        tap_code16(GER_SZ);
-    } else {
-        reset_tap_dance(state);
-    }
-};
-
-void eur_tap_dance(tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-        tap_code(KC_E);
-    } else if (state->count == 2) {
-        tap_code(KC_E);
-    } else if (state->count == 3) {
-        tap_code16(EU_EUR);
-    } else {
-        reset_tap_dance(state);
-    }
-};
-
 tap_dance_action_t tap_dance_actions[] = {
   [TD_F1_F11] = ACTION_TAP_DANCE_DOUBLE(KC_F1, KC_F11),
   [TD_F2_F12] = ACTION_TAP_DANCE_DOUBLE(KC_F2, KC_F12),
-  [TD_UML_AE] = ACTION_TAP_DANCE_FN(auml_tap_dance),
-  [TD_UML_OE] = ACTION_TAP_DANCE_FN(ouml_tap_dance),
-  [TD_UML_UE] = ACTION_TAP_DANCE_FN(uuml_tap_dance),
-  [TD_GER_SZ] = ACTION_TAP_DANCE_FN(ger_sz_tap_dance),
-  [TD_EU_EUR] = ACTION_TAP_DANCE_FN(eur_tap_dance),
+  [TD_UML_AE] = ACTION_TAP_DANCE_DOUBLE(KC_A, UML_AE),
+  [TD_UML_OE] = ACTION_TAP_DANCE_DOUBLE(KC_O, UML_OE),
+  [TD_UML_UE] = ACTION_TAP_DANCE_DOUBLE(KC_U, UML_UE),
+  [TD_GER_SZ] = ACTION_TAP_DANCE_DOUBLE(KC_S, GER_SZ),
+  [TD_EU_EUR] = ACTION_TAP_DANCE_DOUBLE(KC_E, EU_EUR),
   [TD_T_RBRC] = ACTION_TAP_DANCE_DOUBLE(KC_T, KC_RBRC),
   [TD_Y_LBRC] = ACTION_TAP_DANCE_DOUBLE(KC_Y, KC_LBRC)
+};
+
+// Shift + esc = ~
+const key_override_t tilde_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_GRV));
+
+// GUI + esc = `
+const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_GUI, KC_ESC, KC_GRV);
+
+const key_override_t *key_overrides[] = {
+	&tilde_esc_override,
+	&grave_esc_override
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -91,18 +28,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case MC_QUOT:
        if (record->event.pressed) {
            SEND_STRING(SS_TAP(X_QUOT) SS_TAP(X_SPC));
-       }
-       break;
-
-    case MC_GRV:
-       if (record->event.pressed) {
-            if (((get_mods() & MOD_MASK_SHIFT) != 0)) {
-                register_code(KC_LCTL);
-                tap_code(KC_GRV);
-                unregister_code(KC_LCTL);
-            } else if (((get_mods() & MOD_MASK_GUI) != 0)) {
-                SEND_STRING(SS_TAP(X_GRV) SS_TAP(X_SPC));
-            }
        }
        break;
 
