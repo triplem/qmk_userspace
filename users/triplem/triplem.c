2fb338f35ea15de46b7ed7e4571a5b534ab47fe7
triplem <160079+triplem@users.noreply.github.com>
2025-11-20 10:56:17 +0100

    fix: remove rgb matrix layer

--- users/triplem/triplem.c
+++ users/triplem/triplem.c
@@ -45,20 +45,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     return true;
 };
-
-// LAyer Indicator on all keys
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    for (uint8_t i = led_min; i < led_max; i++) {
-        switch(get_highest_layer(layer_state|default_layer_state)) {
-            case 2:
-                rgb_matrix_set_color(i, RGB_BLUE);
-                break;
-            case 1:
-                rgb_matrix_set_color(i, RGB_YELLOW);
-                break;
-            default:
-                break;
-        }
-    }
-    return false;
-}
